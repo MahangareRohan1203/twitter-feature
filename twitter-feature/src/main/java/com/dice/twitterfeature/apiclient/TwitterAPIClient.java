97842a0d49681e3e925c9004627c5c9e4fae9560
MahangareRohan1203 <realme2607@gmail.com>
2024-03-25 23:48:44 +0530

    file moved

--- twitter-feature/src/main/java/com/dice/twitterfeature/apiclient/TwitterAPIClient.java
+++
@@ -1,9 +0,0 @@
-package com.dice.twitterfeature.apiclient;
-
-import com.dice.twitterfeature.response.UserResponse;
-import com.fasterxml.jackson.core.JsonProcessingException;
-
-public interface TwitterAPIClient {
-    public UserResponse getTwitterUsersByName(String name) throws JsonProcessingException;
-    public Object getTwitterTweetsByUserId(String id) throws JsonProcessingException;
-}
